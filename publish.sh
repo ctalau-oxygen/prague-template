96272c73afcdbb3267d918fe276c2c1ba66d8f90
ctalau <cristitalau@gmail.com>
2019-02-02 23:23:58 +0200

    Better generated HTML page

--- publish.sh
+++ publish.sh
@@ -6,7 +6,7 @@ mkdir frameworks
 unzip frameworks-20.1.0.3.zip -d frameworks/
 
 mkdir -p out
-echo "<h1>XML Prague 2019 Paper</h1>" > out/index.html
+echo "<h1>XML Prague 2019 Paper Draft</h1>" > out/index.html
 
 REPO=`echo $REPOSITORY_URL | cut -d '/' -f 5`
 USER=`echo $REPOSITORY_URL | cut -d '/' -f 4`
@@ -16,7 +16,10 @@ do
   sh "oxygen-pdf-chemistry/chemistry.sh" -in "$file" \
     -css frameworks/docbook/css/docbook.css \
     -out "out/${file%.*}.pdf"
-   echo "<a href='${file%.*}.pdf'>View ${file%.*}.pdf</a>" >> out/index.html
    EDIT_URL="https://www.oxygenxml.com/oxygen-xml-web-author/app/oxygen.html?url=gitgh%3A%2F%2Fhttps%253A%252F%252Fgithub.c;om%252F${USER}%252F${REPO}%2F${BRANCH}%2F${file}"
-   echo "<a href='${EDIT_URL}'>Edit ${file%.*}.xml</a>" >> out/index.html
+   echo "<ul>"
+   echo "<li><a href='${file%.*}.pdf'>View generated PDF version</a>" >> out/index.html
+   echo "<li><a href='${EDIT_URL}'>Edit Docbook source</a>" >> out/index.html
+   echo "<li><a href='${REPOSITORY_URL}'>View on GitHub</a>" >> out/index.html
+   echo "</ul>"
 done
