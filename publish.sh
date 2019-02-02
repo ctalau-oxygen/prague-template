a49f7433e2206c8052af2ab0bf4f1c5603d01f51
ctalau <cristitalau@gmail.com>
2019-02-02 23:07:48 +0200

    Added edit link to the output

--- publish.sh
+++ publish.sh
@@ -6,12 +6,14 @@ mkdir frameworks
 unzip frameworks-20.1.0.3.zip -d frameworks/
 
 mkdir -p out
-touch out/index.html
+echo "<h1>XML Prague 2019 Paper</h1>" > out/index.html
 
 for file in `ls *.xml`;
 do
   sh "oxygen-pdf-chemistry/chemistry.sh" -in "$file" \
     -css frameworks/docbook/css/docbook.css \
     -out "out/${file%.*}.pdf"
-   echo "<a href='${file%.*}.pdf'>${file}</a>" >> out/index.html
+   echo "<a href='${file%.*}.pdf'>View ${file%.*}.pdf</a>" >> out/index.html
+   EDIT_URL="https://www.oxygenxml.com/oxygen-xml-web-author/app/oxygen.html?url=${REPOSITORY_URL}/${BRANCH}/${file}"
+   echo "<a href='${EDIT_URL}'>Edit ${file%.*}.xml</a>" >> out/index.html
 done
